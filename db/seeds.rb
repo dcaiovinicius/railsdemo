92aa1d4f156676412adf12f489a1699acdfa9f70
caiovinicius42 <caioviniciusdev@gmail.com>
2022-01-10 23:31:19 -0400

    Set up FFaker and creating a seed for model Friend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'ffaker'
+
+10.times do
+  Friend.create do |friend|
+    friend.first_name = FFaker::Name.name
+    friend.email = FFaker::Internet.email
+    friend.phone = FFaker::PhoneNumberBR.international_mobile_phone_number
+    friend.description = FFaker::DizzleIpsum.phrase
+  end
+end
