15043fad6f9e96adf00abab30dd8fad52ec36ec6
caiovinicius42 <caioviniciusdev@gmail.com>
2022-01-10 23:45:03 -0400

    Showing a friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,18 @@
 class FriendsController < ApplicationController
+  before_action :set_friend, only: %i{ show update destroy edit }
 
   # GET /friends
   def index
     @friends = Friend.all
   end
+
+  # GET /friends/1
+  def show
+  end
+
+  private
+
+    def set_friend
+      @friend = Friend.find(params[:id])
+    end
 end
--- test/controllers/friends_controller_test.rb
+++ test/controllers/friends_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class FriendsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @friend = friends(:one)
+  end
+
   test "should get index" do
     get friends_url
+    assert_response :success
+  end
+
+  test "should show friend" do
+    get friend_url(@friend)
     assert_response :success
   end
 end
